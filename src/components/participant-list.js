6b4afbb6fb4e656b34b8ab151f2820e6268be950
Chett Tiller <chett.tiller@gmail.com>
2019-07-11 14:30:27 -0400

    Sorted cards by alphabetical

--- src/components/participant-list.js
+++ src/components/participant-list.js
@@ -1,18 +1,24 @@
 import React from "react";
 import ParticipantCard from "./participant-card";
 
+function compareByAlphabetical(a, b) {
+  return a > b ? -1 : a < b ? 1 : 0;
+}
+
 const ParticipantList = ({ participants, removeParticipant, setChances }) => {
   const renderParticipantList = () => {
-    return participants.map(par => {
-      return (
-        <ParticipantCard
-          key={par.name}
-          participant={par}
-          removeParticipant={removeParticipant}
-          setChances={setChances}
-        />
-      );
-    });
+    return [...participants]
+      .sort((a, b) => compareByAlphabetical(b.name, a.name))
+      .map(par => {
+        return (
+          <ParticipantCard
+            key={par.name}
+            participant={par}
+            removeParticipant={removeParticipant}
+            setChances={setChances}
+          />
+        );
+      });
   };
 
   if (participants.length) {
