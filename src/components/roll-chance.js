3aa50fba3ebe585d5b87a50fb6601da474a359bb
Chett Tiller <chett.tiller@gmail.com>
2019-07-19 14:57:29 -0400

    Minor refactor

--- src/components/roll-chance.js
+++ src/components/roll-chance.js
@@ -8,19 +8,27 @@ const RollChance = ({
   setDisplayMessage,
   setActiveParticipant
 }) => {
+  const nextChance = availableParticipants => {
+    const activeParticipant = random(availableParticipants);
+    setDisplayMessage(`${activeParticipant.name}'s turn to act!`);
+    setChances(activeParticipant, activeParticipant.chances - 1);
+    setActiveParticipant(activeParticipant);
+    document.title = `${activeParticipant.name}'s Turn`;
+  };
+
+  const startNewRound = () => {
+    setDisplayMessage("Starting a new round...");
+    resetRound();
+    setActiveParticipant(null);
+    document.title = `New Round`;
+  };
+
   const rollForChance = () => {
     const availableParticipants = participants.filter(p => p.chances > 0);
     if (availableParticipants.length) {
-      const activeParticipant = random(availableParticipants);
-      setDisplayMessage(`${activeParticipant.name}'s turn to act!`);
-      setChances(activeParticipant, activeParticipant.chances - 1);
-      setActiveParticipant(activeParticipant);
-      document.title = `${activeParticipant.name}'s Turn`;
+      nextChance(availableParticipants);
     } else if (participants.length) {
-      setDisplayMessage("Starting a new round...");
-      resetRound();
-      setActiveParticipant(null);
-      document.title = `New Round`;
+      startNewRound();
     } else {
       setDisplayMessage("Add participants to start a new round!");
       document.title = `Action Chance!`;
