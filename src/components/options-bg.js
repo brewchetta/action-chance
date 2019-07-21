42d321dfa4b17b53ff1512c1f6b82eb6bd532d74
brewchetta <chett.tiller@gmail.com>
2019-07-21 12:48:13 -0400

    Added files for options

---
+++ src/components/options-bg.js
@@ -0,0 +1,3 @@
+// https://stackoverflow.com/questions/5633264/javascript-get-image-dimensions
+// ^ change the background class depending on the size of a linked image
+// TODO: the idea here is a user will be able to link / temporarily upload an image to be used as a background image
---
+++ src/components/options.js
@@ -0,0 +1,1 @@
+// TODO: The idea here is a user will be able to set various options, starting with the background image
