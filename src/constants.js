a7bff9e974d807c8a956ca8e3c415e901ef28587
Chett Tiller <chett.tiller@gmail.com>
2019-09-06 17:37:34 -0400

    New backend

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,7 @@ export const reconnectionDelay = 2000
 export const reconnectionAttempts = 5
 
 // export const endpoint = '10.9.110.165:3050'
-export const endpoint = 'http://localhost:3050'
+export const endpoint = 'https://action-chance-backend.herokuapp.com/'
 
 export const debugLog = message => {
   if (process.env.NODE_ENV !== 'production') console.log(`%cDebug: ${message}`, 'color: blue')
