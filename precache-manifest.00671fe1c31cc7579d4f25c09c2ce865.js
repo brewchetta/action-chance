be6642616afcf48af12311536fd668a1d365fd91
Chett Tiller <chett.tiller@gmail.com>
2019-09-06 14:15:41 -0400

    Updates

---
+++ precache-manifest.00671fe1c31cc7579d4f25c09c2ce865.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda3a20e6e7e2778b58f818dfbe46083",
+    "url": "/action-chance/index.html"
+  },
+  {
+    "revision": "45bfa33b995b1bb35cd3",
+    "url": "/action-chance/static/css/main.01bebaf0.chunk.css"
+  },
+  {
+    "revision": "a24b4fc30d016462845c",
+    "url": "/action-chance/static/js/2.50653ef7.chunk.js"
+  },
+  {
+    "revision": "45bfa33b995b1bb35cd3",
+    "url": "/action-chance/static/js/main.fb1415f3.chunk.js"
+  },
+  {
+    "revision": "32e5a9b10e482348a285",
+    "url": "/action-chance/static/js/runtime~main.8b254408.js"
+  }
+]);
