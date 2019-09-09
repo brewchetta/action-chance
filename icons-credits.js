da7c419d41d6e2dca5ac7da617f4e9c8ed738233
Chett Tiller <chett.tiller@gmail.com>
2019-09-09 12:17:04 -0400

    Added a shit load of images

---
+++ icons-credits.js
@@ -0,0 +1,67 @@
+export const iconCredits = {
+  "Game-icons.net": {
+    Lorc: [
+      'Arrow dunk',
+      'Snake',
+      'Potion ball',
+      'Rolling bomb',
+      'Swords emblem',
+      'Blade drag',
+      'Hood',
+      'High shot',
+      'Pouring chalice',
+      'Tree branch',
+      'Fluffy cloud',
+      'Candle holder',
+      'Camp fire',
+      'Snowflake 2',
+      'Lightning storm',
+      'Tombstone',
+      'Spectre',
+      'Witch flight',
+      'Treasure map',
+      'Key',
+      'Crossed chains',
+      'Cash',
+      'Bubbles',
+      'Anchor',
+      'Shiny Apple',
+      'Hazard sign',
+      'Angel wings',
+      'Broken shield',
+      'Gears',
+      'Empty hourglass',
+      'Brain',
+      'Lightning tear',
+      'Magnifying glass'
+    ],
+    Delapouite: [
+      'Holy water',
+      'Moon bats',
+      'Night sleep',
+      'Torch',
+      'Backward time',
+      'Plain arrow',
+      'Stone pile',
+      'Book cover',
+      'Sheep',
+      'Liberty wing',
+      'Shamrock',
+      'Perspective dice 6 faces 6',
+      'Horseshoe',
+      'Eyelashes'
+    ],
+    Skoll: [
+      'Fangs',
+      'Footstep',
+      'Pentacle'
+    ],
+    'CarlOlsen': [
+      'Flame'
+    ],
+    sbed: [
+      'Fireflake',
+      'Electric'
+    ]
+  }
+}
