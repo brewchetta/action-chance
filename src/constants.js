b1e5f0bf7a1780b592161ed20706e609035d7806
Chett Tiller <chett.tiller@gmail.com>
2019-09-09 12:33:06 -0400

    Set up isProduction variable and split backends

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,13 @@
+export const isProduction = process.env.NODE_ENV === 'production'
+
 export const defaultBGImage = "https://img3.goodfon.com/wallpaper/nbig/1/be/art-fantasy-forest-dragon-by.jpg"
 
 export const reconnectionDelay = 2000
 
 export const reconnectionAttempts = 5
 
-// export const endpoint = '10.9.110.165:3050'
-export const endpoint = 'https://action-chance-backend.herokuapp.com/'
+export const endpoint = isProduction ? 'https://action-chance-backend.herokuapp.com/' : '10.9.110.165:3050'
 
 export const debugLog = message => {
-  if (process.env.NODE_ENV !== 'production') console.log(`%cDebug: ${message}`, 'color: blue')
+  if (isProduction) console.log(`%cDebug: ${message}`, 'color: blue')
 }
