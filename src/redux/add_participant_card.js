a3d47068ffbd9c7b942fcb830176568adf7677f9
Chett Tiller <chett.tiller@gmail.com>
2019-12-16 16:47:41 -0500

    Added local storage options for initiative and add cards

--- src/redux/add_participant_card.js
+++ src/redux/add_participant_card.js
@@ -1,9 +1,20 @@
 import { TOGGLE_ADD_PARTICIPANT_CARD } from './actions'
 
+// Get local storage if it exists
+const localState = () => {
+  if (localStorage.options) {
+    return JSON.parse(localStorage.options).addParticipantCard
+  }
+
+  localStorage.options = JSON.stringify({utilizeInitiative: 1, addParticipantCard: true})
+  return true
+}
+
 // addParticipantCard = true
-export default function(state = true, action) {
+export default function(state = localState(), action) {
   switch (action.type) {
     case TOGGLE_ADD_PARTICIPANT_CARD:
+      localStorage.options = JSON.stringify({utilizeInitiative, addParticipantCard: !state})
       return !state
     default:
       return state
--- src/redux/utilize_initiative.js
+++ src/redux/utilize_initiative.js
@@ -1,9 +1,20 @@
 import { SET_UTILIZE_INITIATIVE } from './actions'
 
+// Get local storage if it exists
+const localState = () => {
+  if (localStorage.options) {
+    return JSON.parse(localStorage.options).utilizeInitiative
+  }
+
+  localStorage.options = JSON.stringify({utilizeInitiative: 1, addParticipantCard: true})
+  return true
+}
+
 // utilizeInitiative = 1
-export default function(state = 1, action) {
+export default function(state = localState(), action) {
   switch (action.type) {
     case SET_UTILIZE_INITIATIVE:
+      localStorage.options = JSON.stringify({utilizeInitiative: action.payload, addParticipantCard})
       return action.payload
     default:
       return state
