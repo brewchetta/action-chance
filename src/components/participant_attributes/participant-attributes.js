4f5d737fc2f5f76712456c2b826b326ea4763ae2
Chett Tiller <chett.tiller@gmail.com>
2020-01-16 15:42:02 -0500

    Added plus button to add tags

--- src/components/participant_attributes/participant-attributes.js
+++ src/components/participant_attributes/participant-attributes.js
@@ -7,17 +7,37 @@ const pngs = require.context('../../assets/attribute_icons', true, /\.png/)
 
 /*------Component------*/
 const ParticipantAttributes = ({ attributes, removeAttribute }) => {
+
+  const renderAttributes = () => {
+    const attrs = attributes.map((attr, i) => (
+      <button
+        className="attribute-button"
+        key={i}
+        name={attr}
+        onClick={() => removeAttribute(attr)}
+      >
+        <img src={pngs(attr) ? pngs(attr) : '?'} alt='' />
+      </button>
+    ))
+    attrs.push(
+        <button
+        className="attribute-button"
+        key={-1}
+        name={"add-attribute"}
+        onClick={() => console.log("behavior here")}
+      >
+        <img src={'http://pluspng.com/img-png/free-png-plus-sign-plus-icon-512.png'} style={{filter: "invert(1)"}} alt='stuff' />
+      </button>
+    )
+    return attrs
+  }
+  
   /*------Render------*/
-  return attributes.map((attr, i) => (
-    <button
-      className="attribute-button"
-      key={i}
-      name={attr}
-      onClick={() => removeAttribute(attr)}
-    >
-      <img src={pngs(attr) ? pngs(attr) : '?'} alt='' />
-    </button>
-  ));
+  return (
+    <>
+      {renderAttributes()}
+    </>
+  )
 };
 
 export default ParticipantAttributes;
